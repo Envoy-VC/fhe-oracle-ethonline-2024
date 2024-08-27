81f98a42505a0947de8bd5a53dc21b4744b12805
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-28 01:08:20 +0530

    chore: add Oracle Client Interface

---
+++ packages/oracle/src/interfaces/IOracleClient.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @title FHE Oracle client interface.
+interface IFunctionsClient {
+    /// @notice Oracle response handler called by the Oracle Router
+    /// @param requestId The requestId returned by FunctionsClient.sendRequest().
+    /// @param response Aggregated response from the request's source code.
+    /// @param error Aggregated error either from the request's source code or from the execution pipeline.
+    /// @dev Either response or error parameter will be set, but never both.
+    function handleOracleFulfillment(bytes32 requestId, bytes memory response, bytes memory error) external;
+}
